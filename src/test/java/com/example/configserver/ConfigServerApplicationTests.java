c2eda3745c3fa51f39a30c6f4508d444f47e629b
ddr34 <deepaksargar662@gmail.com>
2023-07-24 22:24:45 +0530

    Update ConfigServerApplicationTests.java

--- src/test/java/com/example/configserver/ConfigServerApplicationTests.java
+++ src/test/java/com/example/configserver/ConfigServerApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.configserver;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,30 @@ class ConfigServerApplicationTests {
 
     @Test
     void contextLoads() {
+        fail("fail");
+    }
+
+    @Test
+    void contextLoads1() {
+        fail("fail1");
+    }
+
+    @Test
+    void contextLoads2() {
+        fail("fail2");
+    }
+
+    @Test
+    void contextLoads3() {
+        fail("fail3");
+    }
+
+    @Test
+    void contextLoads4() {
+    }
+
+    @Test
+    void contextLoads5() {
     }
 
 }
